e32de0e31475603b657612925c9e059794522a99
arazo <jo.ar@lawshop.io>
2021-10-26 09:57:57 +0900

    :sparkles: WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,30 @@
-alert("hi");
+
+// backend와 connection 열어줌
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+    console.log("Connected to Server");
+}
+
+function handleMessage(message) {
+    console.log("New message : ", message.data);
+}
+
+function handleClose() {
+    console.log("Disconnected from the Server");
+}
+
+// socket이 열렸을때 실행
+socket.addEventListener("open", handleOpen);
+
+// backend에서 message를 받았을때 실행
+socket.addEventListener("message", handleMessage);
+
+// backend와의 연결이 끊겼을때 실행
+socket.addEventListener("close", handleClose);
+
+setTimeout(() => {
+    // frontend에서 backend로 message 보냄
+    // buffer로 보내짐 나중에 수정 예정
+    socket.send("hello from th browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -10,4 +12,30 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser")
+}
+
+function onSocketMessage(message) {
+    console.log(message);
+}
+
+// connection이 생기면 socket에서 누가 연결했는지 알 수 있음
+// 새로운 브라우저가 내 서버에 들어오면 실행됨
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+
+    // 브라우저 탭을 닫거나 컴퓨터가 잠자기 모드에 들어가면 이벤트 발생
+    socket.on("close", onSocketClose);
+
+    // 특정 socket에서 메시지를 받았을때 발생
+    socket.on("message", onSocketMessage);
+
+    // 브라우저에 message보냄
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
